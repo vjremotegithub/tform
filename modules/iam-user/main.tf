040fc5d6ae745f633cf985a7631181895cedf268
Vijay Jadhav <Vijay Jadhav@DESKTOP-RCSRL0M>
2018-07-10 11:40:42 +0100

    .

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "${var.aws_region}"
   #shared_credentials_file = "${var.shared_credentials_file}"
   #profile="${var.aws_profile}"
+  profile                 = "snd-profile"
+  shared_credentials_file= "/home/vijay/.aws/credentials"
+}
+
+provider "aws" {
+  region                  = "eu-west-1"
+  shared_credentials_file = "~/.aws/credentials
+  profile                 = "snd-profile"
 }
 
 terraform {
