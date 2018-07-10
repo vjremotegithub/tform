874a779c08028fe0acf139489b4888aae388baa4
Vijay Jadhav <Vijay Jadhav@DESKTOP-RCSRL0M>
2018-07-10 11:18:44 +0100

    Back end added

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -4,6 +4,10 @@ provider "aws" {
   profile="${var.aws_profile}"
 }
 
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
 
 resource "aws_iam_user" "test_user" {
     name = "${var.name}"
