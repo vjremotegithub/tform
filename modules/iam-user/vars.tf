63c992c07a881b90d605f44740f2292c22f44755
Vijay Jadhav <Vijay Jadhav@DESKTOP-RCSRL0M>
2018-07-11 08:00:03 +0100

    .

--- modules/iam-user/vars.tf
+++ modules/iam-user/vars.tf
@@ -6,12 +6,4 @@ variable "aws_region" {
   description = "The AWS region to deploy to (e.g. us-east-1)"
 }
 
-variable "aws_profile" {
-  description = "The AWS profile"
-}
-
-variable "shared_credentials_file" {
-  description = "The AWS credential file if using profiles"
-}
-
 
