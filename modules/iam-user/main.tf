a4da1b1a2c90b29caa7bf48fc0c972b26de05bb6
Vijay Jadhav <Vijay Jadhav@DESKTOP-RCSRL0M>
2018-07-12 10:18:53 +0100

    .

--- modules/iam-user/main.tf
+++ modules/iam-user/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "${var.aws_region}"
   profile= "${var.aws_profile}"
-  shared_credentials_file= "/home/vijay/.aws/credentials"
+  shared_credentials_file= "~/.aws/credentials"
 }
 
 
